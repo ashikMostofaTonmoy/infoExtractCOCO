ee0ba278db34835a2b4323a2eb952e7b65795cde
ashikMostofaTonmoy <tonmoy.ashik@gmail.com>
2022-06-27 16:58:53 +0600

    file merge

--- excelfilemerge.py
+++ excelfilemerge.py
@@ -7,11 +7,59 @@ print(platform.python_version())
 print(sys.path)
 
 # %%
-sidecsv = pd.read_csv('data/COCO_Side.csv', index_col=0)
-sidecsv
+# sideKPcsv = pd.read_csv('data/COCO_Side.csv', index_col='name')
+sideKPcsv = pd.read_csv('data/COCO_Side.csv', index_col=0)
+sideKPcsv
 
 # %%
-hard_features = pd.read_csv(
-    'data/input/hard_features_With_weight_v2.csv', index_col=0)
-hard_features
+side_SEGCSV = pd.read_csv(
+    'data/reorganized/segmentation_extracted.csv', index_col=0)
+side_SEGCSV['name'] = side_SEGCSV.name.map(str)+'.jpg'
+side_SEGCSV.keys()
+side_SEGCSV
+# %%
+
+rearKPcsv = pd.read_csv('data/COCO_B3_rear.csv', index_col=0)
+rearKPcsv.keys()
+# rearKPcsv
+
+# print(rearKPcsv)
+
+# %%
+for entry in side_SEGCSV.index:
+    try:
+        name = side_SEGCSV.loc[entry, 'name']
+        # side_height =
+        # scaleCompare = pd.DataFrame(list(zip(name, side_height)),
+        #                     columns=['name', 'side_height'])
+        # scaleCompare = pd.DataFrame(list(name, side_height),
+        #             columns=['name', 'side_height'])
+        info = {
+            'id': [entry],
+            'name': [name],
+            'side_height': [sideKPcsv.loc[name, 'side_height']]
+
+        }
+        # print(f"{name} {side_height}")
+        print(info)
+    except:
+        pass
+# %%
+side_seg_kp = pd.merge(sideKPcsv, side_SEGCSV, how='outer', on=['name'])
+side_seg_kp.keys()
+
+# %%
+
+# rearKPcsv.rename(index=str, columns={'Reader_ID1': 'Reader_ID1_x',
+#            'SITE_ID1': 'SITE_ID1_x', 'EVENT_TS1': 'EVENT_TS1_x'}, inplace=True)
+# all_merrged = pd.merge(side_seg_kp, side_SEGCSV, how='outer', on=['name'])
+
+# %%
+# rearKPcsv['name'].str.split('_').str[3]
+rearKPcsv['only_name'] = rearKPcsv['name'].str.split(
+    '_').str[0]+'_'+rearKPcsv['name'].str.split('_').str[2]+'_'+rearKPcsv['name'].str.split('_').str[3]
+
+# %%
+# +rearKPcsv['name'].str.split('_')[2]+'_'+rearKPcsv['name'].str.split('_')[3]
+rearKPcsv['only_name']
 # %%
